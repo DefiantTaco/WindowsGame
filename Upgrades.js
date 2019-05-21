8223356641d131f69391421591dc939b61dad332
DefiantTaco <45100576+DefiantTaco@mackan_lindback@hotmail.com>
2019-05-21 12:50:59 +0200

    Last Update

--- Upgrades.js
+++ Upgrades.js
@@ -3,11 +3,30 @@ var SlimeFactory = 0;
 var SlimeGalactica = 0;
 var SlimeExplosive = 0;
 var SlimeRad = 0;
+var SlimeClick = 0;
+var SlimeClickExplosive = 0;
 var dpsAuto = 0;
 var dpsGalactica = 0;
 var dpsFactory = 0;
 var dpsExplosive = 0;
 var dpsRad = 0;
+var Click = 1;
+
+function buySlimeClick(){
+    if (cookiecount >= 200) {
+      cookiecount = cookiecount - 200;
+      Click = Click * 2;
+      document.getElementById("SlimeClick").style.display = "none";
+    }
+}
+
+function buySlimeExplosiveClick(){
+    if (cookiecount >= 500) {
+      cookiecount = cookiecount - 500;
+      Click = Click * 2;
+      document.getElementById("SlimeExplosiveClick").style.display = "none";
+    }
+}
 
 function buyAutoClick(){
   if (cookiecount >= ((autoClick+1) * 3)) {
@@ -39,7 +58,7 @@ function buySlimeGalactia(){
   }
 }
 
-  function buySlimeExplosive(){
+function buySlimeExplosive(){
     if (cookiecount >= ((SlimeExplosive+1) * 500)) {
       cookiecount = cookiecount - ((SlimeExplosive+1) * 500);
       SlimeExplosive = SlimeExplosive + 1;
@@ -48,7 +67,8 @@ function buySlimeGalactia(){
       update();
     }
   }
-    function buySlimeRad(){
+
+function buySlimeRad(){
       if (cookiecount >= ((SlimeRad+1) * 1500)) {
         cookiecount = cookiecount - ((SlimeRad+1) * 1500);
         SlimeRad = SlimeRad + 1;
